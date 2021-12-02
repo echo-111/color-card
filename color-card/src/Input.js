2b729715ac2dfbcdbc00ef02f4d25a67571b7ba3
fan <fanyoung3472@gmail.com>
2021-12-02 22:04:00 +1100

    add: color name

--- color-card/src/Input.js
+++ color-card/src/Input.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React from 'react';
+import colorNames from 'colornames';
 
-const Input = ({ colorValue, setColorValue }) => {
+const Input = ({ 
+    colorValue, setColorValue, setHexValue, isDarkText, setIsDarkText 
+}) => {
     return (
         <form onSubmit={(e) => e.preventDefault()}>
             <label>Add Color Name:</label>
@@ -10,8 +13,17 @@ const Input = ({ colorValue, setColorValue }) => {
                 placeholder="Add Color Name"
                 required
                 value={colorValue}
-                onChange={(e) => setColorValue(e.target.value)}
+                onChange={(e) => {
+                    setColorValue(e.target.value);
+                    setHexValue(colorNames(e.target.value));           
+                }}
             />
+            <button
+                type="button"
+                onClick={() => setIsDarkText(!isDarkText)}
+            >
+                Toggle text color
+            </button>
 
         </form>
     )
